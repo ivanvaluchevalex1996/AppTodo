4a11372d055cee8861de57b2cddf9e241f1ccf33
ivan <ivanvaluchevalex1996@gmail.com>
2022-11-04 18:00:32 +0300

    asdqweqw

--- src/components/NewTaskForm/newTaskForm.jsx
+++ src/components/NewTaskForm/newTaskForm.jsx
@@ -1,63 +1,79 @@
-import React from "react";
+// import React from "react";
+// import "./newTaskForm.css";
+
+// export default class NewTaskForm extends React.Component {
+//   constructor(props) {
+//     super(props);
+
+//     this.state = {
+//       label: "",
+//     };
+//   }
+
+//   onLableChange = (event) => {
+//     this.setState({
+//       label: event.target.value,
+//     });
+//   };
+
+//   onSubmit = (event) => {
+//     event.preventDefault();
+//     // this.props.onItemAdd(this.state.label);
+//     const { onItemAdd } = this.props;
+//     const { label } = this.state;
+//     onItemAdd(label);
+//     this.setState({
+//       label: "",
+//     });
+//   };
+
+//   render() {
+//     const { label } = this.state;
+
+//     return (
+// <header className="header">
+//   <form onSubmit={this.onSubmit}>
+//     <h1>todos</h1>
+//     <input
+//       className="new-todo"
+//       placeholder="What needs to be done?"
+//       onChange={this.onLableChange}
+//       value={label}
+//     />
+//   </form>
+// </header>
+//     );
+//   }
+// }
+
+import React, { useState } from "react";
 import "./newTaskForm.css";
 
-export default class NewTaskForm extends React.Component {
-  constructor(props) {
-    super(props);
+function NewTaskForm({ onItemAdd }) {
+  const [label, setLabel] = useState("");
 
-    this.state = {
-      label: "",
-    };
+  function onLableChange(event) {
+    return setLabel(event.target.value);
   }
-
-  onLableChange = (event) => {
-    this.setState({
-      label: event.target.value,
-    });
-  };
-
-  onSubmit = (event) => {
+  function onSubmit(event) {
     event.preventDefault();
-    // this.props.onItemAdd(this.state.label);
-    const { onItemAdd } = this.props;
-    const { label } = this.state;
     onItemAdd(label);
-    this.setState({
-      label: "",
-    });
-  };
-
-  render() {
-    const { label } = this.state;
-
-    return (
-      <header className="header">
-        <form onSubmit={this.onSubmit}>
-          <h1>todos</h1>
-          <input
-            className="new-todo"
-            placeholder="What needs to be done?"
-            onChange={this.onLableChange}
-            value={label}
-          />
-        </form>
-      </header>
-    );
+    setLabel("");
   }
+
+  return (
+    <header className="header">
+      <form onSubmit={onSubmit}>
+        <h1>todos</h1>
+        <input
+          className="new-todo"
+          placeholder="What needs to be done?"
+          onChange={onLableChange}
+          value={label}
+        />
+      </form>
+    </header>
+  );
 }
 
-// const NewTaskForm = () => {
-//   return (
-//     <header className="header">
-//       <h1>todos</h1>
-//       <input
-//         className="new-todo"
-//         placeholder="What needs to be done?"
-//         autoFocus
-//         onChange={this.onLableChange}
-//       />
-//     </header>
-//   );
-// };
-
-// export default NewTaskForm;
+export default NewTaskForm;
