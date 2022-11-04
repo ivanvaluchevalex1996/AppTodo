c915502809cc1a44b317f17e614c6a6b505b7947
ivan <ivanvaluchevalex1996@gmail.com>
2022-11-04 18:01:28 +0300

    qwedzxc

--- src/components/TasksFilter/tasksFilter.jsx
+++ src/components/TasksFilter/tasksFilter.jsx
@@ -1,34 +1,3 @@
-// import React from "react";
-// import "./tasksFilter.css";
-
-// export default class TasksFilter extends React.Component {
-//   buttons = [
-//     { name: "all", label: "All" },
-//     { name: "active", label: "Active" },
-//     { name: "done", label: "Done" },
-//   ];
-
-//   render() {
-//     const { filter, onFilterChange } = this.props;
-//     const buttons = this.buttons.map(({ name, label }) => {
-//       const isActive = filter === name;
-//       const clazz = isActive ? "selected" : "button";
-//       return (
-//         <button
-//           type="button"
-//           key={name}
-//           className={`${clazz}`}
-//           onClick={() => onFilterChange(name)}
-//         >
-//           {label}
-//         </button>
-//       );
-//     });
-
-//     return <div className="filters">{buttons}</div>;
-//   }
-// }
-
 import React from "react";
 import "./tasksFilter.css";
 
